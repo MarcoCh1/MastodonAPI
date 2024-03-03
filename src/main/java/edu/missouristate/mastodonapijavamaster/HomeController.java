c75ce77e3f060b76e4fa89bfd1b17901fe529a0a
Kyan Carr <kyancarr.51202@outlook.com>
2024-03-03 14:47:59 -0600

    initial commit

---
+++ src/main/java/edu/missouristate/mastodonapijavamaster/HomeController.java
@@ -0,0 +1,13 @@
+package edu.missouristate.mastodonapijavamaster;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/java/edu/missouristate/mastodonapijavamaster/MastodonApiJavaMasterApplication.java
@@ -0,0 +1,13 @@
+package edu.missouristate.mastodonapijavamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastodonApiJavaMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MastodonApiJavaMasterApplication.class, args);
+    }
+
+}
